c16a57e3e668438609eb61524a0468e07605da8a
Jaber Kibria <mhsjaber@gmail.com>
2016-12-11 11:47:21 +0600

    Add project files.

---
+++ MVC/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Monopura" };
+            return View(movie);
+        }
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id = " + Id);
+        }
+        public ActionResult Index(int? PageIndex, string SortBy)
+        {
+            if (!PageIndex.HasValue) {
+                PageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(SortBy)) {
+                SortBy = "Name";
+            }
+            return Content("Page Index = " + PageIndex + ", Sort By = " + SortBy);
+        }
+    }
+}
