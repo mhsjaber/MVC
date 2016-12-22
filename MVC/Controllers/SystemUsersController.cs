a69778f2884e05746587c34ea1752e08631a9156
mhsjaber <mhsjaber@gmail.com>
2016-12-23 00:44:01 +0600

    UI Fixed

---
+++ MVC/Controllers/SystemUsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class SystemUsersController : Controller
+    {
+        private GarmentsManagementEntities db = new GarmentsManagementEntities();
+
+        // GET: SystemUsers
+        public ActionResult Index()
+        {
+            var systemUsers = db.SystemUsers.Include(s => s.SystemUser1);
+            return View(systemUsers.ToList());
+        }
+
+        // GET: SystemUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Create
+        public ActionResult Create()
+        {
+            ViewBag.UpdatedBy = new SelectList(db.SystemUsers, "Id", "UserName");
+            return View();
+        }
+
+        // POST: SystemUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,Password,FullName,UpdateDate,UpdatedBy,Status")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SystemUsers.Add(systemUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UpdatedBy = new SelectList(db.SystemUsers, "Id", "UserName", systemUser.UpdatedBy);
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UpdatedBy = new SelectList(db.SystemUsers, "Id", "UserName", systemUser.UpdatedBy);
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Password,FullName,UpdateDate,UpdatedBy,Status")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(systemUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UpdatedBy = new SelectList(db.SystemUsers, "Id", "UserName", systemUser.UpdatedBy);
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SystemUser systemUser = db.SystemUsers.Find(id);
+            db.SystemUsers.Remove(systemUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
